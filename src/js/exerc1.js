437b486f2b8b2ef9ae6539d9e99d0084393b4de7
Daniel <danieloliveiradesouza09@outlook.com>
2025-04-18 17:01:49 -0300

    commit inicial + exerc1

---
+++ src/js/exerc1.js
@@ -0,0 +1,9 @@
+let vinho1;
+let vinho2 = null;
+let vinho3 = "vinho branco";
+
+console.log(`
+    Undefined -> ${typeof(vinho1)}
+    Vázio - > ${typeof(vinho2)}
+    Com valor -> ${typeof(vinho3)}
+`)
